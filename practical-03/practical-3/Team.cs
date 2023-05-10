aaf6d6d4bf41a8126c7a745e3b4f42d40d995ef6
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-10 20:06:32 +0530

    added practical 3

---
+++ practical-03/practical-3/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Team : Sponsor
+    {
+        private string teamName;
+
+        public string PrintInfo()
+        {
+            return owner + " Sponser";
+        }
+    }
+}
